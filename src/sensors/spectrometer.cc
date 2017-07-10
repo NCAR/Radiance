cd7bc6f5b7aa392e4858899f25d4ad503a5c8d2c
dgomes2 <diego.t.gomes.dtg@gmail.com>
2017-07-10 11:34:03 -0600

    Undid accidental merge changes.

--- src/sensors/spectrometer.cc
+++ src/sensors/spectrometer.cc
@@ -98,7 +98,6 @@ namespace RADIANCE{
 
     // Get lambdas from device
     if (AVS_GetLambda(handle_,d_spectrum)!=ERR_SUCCESS) {
-
       std::cerr << "Err in GetLambda" << std::endl; //DEBUG
       return false;
     }
@@ -106,9 +105,6 @@ namespace RADIANCE{
      // Get pixelvals from device
     if (AVS_GetScopeData(handle_,d_pixelval)!=ERR_SUCCESS) {
       std::cerr << "Err in GetScopeData" << std::endl; //DEBUG
-
-      std::cerr << "Err in GetScopeData" << std::endl; //DEBUG
-
       return false;
     }
 
