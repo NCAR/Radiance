6cfb8defed45c7e637738ef58428ef8c2f8e8893
dgomes2 <diego.t.gomes.dtg@gmail.com>
2017-07-18 14:39:31 -0600

    More test debugging.

--- src/sensors/spectrometer.cc
+++ src/sensors/spectrometer.cc
@@ -123,7 +123,7 @@ namespace RADIANCE{
     }
 	  
 	  
-	std::cout << spec_calib_.m_Irradiance.m_IntensityCalib.m_aCalibConvers << std::endl;
+	std::cout << m_Irradiance.m_IntensityCalib.m_aCalibConvers << std::endl;
 	//std::cout << spec_calib_.m_Irradiance.m_IntensityCalib.m_Callnttime << std::endl;
 
     return true;
