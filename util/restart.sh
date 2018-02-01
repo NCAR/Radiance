3c6555a0517c0cf086afe60a3d211a975a6b3ac4
Phil Oakley <phil.oakley@gmail.com>
2018-02-01 13:50:12 -0700

    Updated restart.sh to include the filesize condition.

--- util/restart.sh
+++ util/restart.sh
@@ -10,3 +10,15 @@ RESULT=`pgrep ${PROCESS}`
 if [ "${RESULT:-null}" = null ]; then
     sudo reboot
 fi
+
+FILENAME=$(ls /mnt/slcdrive/ -t | head -n1)
+FILESIZE=$(stat --printf="%s" /mnt/slcdrive/$FILENAME)
+# echo $FILESIZE
+sleep 6
+FILENAME=$(ls /mnt/slcdrive/ -t | head -n1)
+FILESIZE2=$(stat --printf="%s" /mnt/slcdrive/$FILENAME)
+#CHANGE=$(($FILESIZE2-$FILESIZE))
+# echo $CHANGE
+if [ "$FILESIZE" -eq "$FILESIZE2" ]; then
+	sudo reboot
+fi
