f752d038730156f79bf745e28fbcfa4e77804f5e
Phil Oakley <phil.oakley@gmail.com>
2018-04-09 13:21:53 -0600

    Updated display and added read for exposure time
    Updated display and added read for exposure time

--- DIMS_datareader.py
+++ DIMS_datareader.py
@@ -16,7 +16,7 @@ def getFiles():
     #files = "C:\\HAO-IG\\DIMS\\Software\\Data\\sun_angled\\"
     #files = "C:\\HAO-IG\\DIMS\\Software\\Data\\datafile_7-12-2017_1000ms"
     #directory = "C:\\HAO-IG\\DIMS\\Software\\Data\\omega_thermal_vac\\"
-    directory = "C:\\HAO-IG\\DIMS\\Software\\Data\\3-1-2018\\"
+    directory = "C:\\HAO-IG\\DIMS\\Software\\Data\\8-28-2017\\"
     files=os.listdir(directory)
     files = [directory + s for s in files]    
     return(files)    
@@ -28,7 +28,8 @@ def getData(files):
 
     #length of data from 1 cycle. 
     #Need to figure out how to make this automatic    
-    cycleSize = 8240+2048*4+2
+    #last 4 is for the exposure time
+    cycleSize = 8240+2048*4+2+4
  
     #Total file size
     fileSize=0
@@ -36,12 +37,13 @@ def getData(files):
         fileSize += os.path.getsize(file)
 
     #number of data cycles
-    numCycles = fileSize/cycleSize #* len(files)      
+    numCycles = int(fileSize/cycleSize) #* len(files)      
         
     print(cycleSize,fileSize,numCycles)
 
 	#Initilize everything
     timestamp= np.zeros(numCycles)
+    times=np.zeros(numCycles)
     wavelength=np.zeros([numCycles,2048])
     spectrum= np.zeros([numCycles,2048])
     #wavelength = []
@@ -54,6 +56,7 @@ def getData(files):
     slc_temp=np.zeros(numCycles)
     env_temp=np.zeros(numCycles)
     env_hum=np.zeros(numCycles)
+    exposure=np.zeros(numCycles)
     ads1=np.zeros(numCycles)
     ads2=np.zeros(numCycles)
     ads3=np.zeros(numCycles)
@@ -68,6 +71,7 @@ def getData(files):
     for file in files:
         #Total file size
         fileSize = os.path.getsize(file)
+        print(file)
         with open(file,'rb') as f:
         
             cycleStartByte = 0 #used to start at offset, not sure why we'd skip the 1st cycle?
@@ -76,9 +80,11 @@ def getData(files):
             
     
             
-            while cycleStartByte < (fileSize-cycleSize):
+            while cycleStartByte < (fileSize):#-cycleSize):
                 f.seek(cycleStartByte, 0)
                 timestamp[cycleCount], = (struct.unpack('I',f.read(4)))
+                times[cycleCount] = timestamp[cycleCount]-timestamp[0]
+                #print(timestamp[cycleCount])
                 
                 #wavelength.append(struct.unpack('2048f',f.read(2048*4)))
                 wavelength[cycleCount,:] = np.asarray(struct.unpack('2048f',f.read(2048*4)))
@@ -92,6 +98,7 @@ def getData(files):
                 slc_temp[cycleCount], = (struct.unpack('f',f.read(4)))
                 env_temp[cycleCount], = (struct.unpack('f',f.read(4)))
                 env_hum[cycleCount], = (struct.unpack('f',f.read(4)))
+                exposure[cycleCount], = (struct.unpack('f',f.read(4)))
     
                 ads1[cycleCount], = (struct.unpack('f',f.read(4)))
                 ads2[cycleCount], = (struct.unpack('f',f.read(4)))
@@ -105,7 +112,6 @@ def getData(files):
                 cycleStartByte = cycleStartByte + cycleSize
                 cycleCount += 1
     
-    
 #            print(bat1_temp)     
 #            print(ads3)     
 #            print(ads4)     
@@ -113,36 +119,37 @@ def getData(files):
             #wavelength = np.asarray(wavelength)        
             #spec_temp = spec_temp[1:]
             #spec_temp = np.array(spec_temp)
-    dataStructure = {'times':timestamp,'spec_temp':spec_temp,'computer_temp':rpi_temp,'battery1_temp':bat1_temp,'battery2_temp':bat2_temp,'slc_temp':slc_temp,'env_temp':env_temp,'env_hum':env_hum,'ads1':ads1,'ads2':ads2,'ads3':ads3,'ads4':ads4,'spec_heater':spectrometer_heater_status,'batt_heater':battery_heater_status}
+    dataStructure = {'timestamp':timestamp,'times':times,'spec_temp':spec_temp,'computer_temp':rpi_temp,'battery1_temp':bat1_temp,'battery2_temp':bat2_temp,'slc_temp':slc_temp,'env_temp':env_temp,'env_hum':env_hum,'exposure':exposure,'ads1':ads1,'ads2':ads2,'ads3':ads3,'ads4':ads4,'spec_heater':spectrometer_heater_status,'batt_heater':battery_heater_status}
     data = pd.DataFrame(dataStructure)
     return(data,wavelength,spectrum)
         
 def extractData():
     pass
 
-def plotTemperatures(data):
+def plotTemperatures(data,ax):
     
     heater_on = 19#1   
     heater_off = 21#3    
-    fig,(ax1)=plt.subplots()    
     times = data['times']
-    times=np.array(times - times[0]) 
-    nonzero=np.where(times>0)
-    timelimits = times[nonzero]
-    ax1.plot(times,data['spec_temp'],label='Spectrometer Temperature')
-    ax1.plot(times,data['computer_temp'],label='Computer Temperature')
-    ax1.plot(times,data['battery1_temp'],label='Battery 1 Temperature')
-    ax1.plot(times,data['battery2_temp'],label='Battery 2 Temperature')
+    #times=np.array(times - times[0]) 
+    #nonzero=np.where(times>0)
+    #timelimits = data['times'][nonzero]
+    ax.plot(data['times'],data['spec_temp'],label='Spectrometer Temperature')
+    ax.plot(data['times'],data['computer_temp'],label='Computer Temperature')
+    ax.plot(data['times'],data['battery1_temp'],label='Battery 1 Temperature')
+    ax.plot(data['times'],data['battery2_temp'],label='Battery 2 Temperature')
     #ax1.plot(data['slc_temp'],label='SLC Temperature')
-    ax1.plot(times,data['spec_heater']*10,label='Spectrometer Heater',linestyle='-',color='b')
-    ax1.plot(times,data['batt_heater']*5,label='Battery Heater',linestyle='-',color='c')
-    ax1.plot([0,np.amax(times)],[heater_on,heater_on],'--g',label='Heater Set Point On')
-    ax1.plot([0,np.amax(times)],[heater_off,heater_off],'--r',label='Heater Set Point Off')
+    ax.plot(data['times'],data['spec_heater']*10,label='Spectrometer Heater',linestyle='-',color='b')
+    ax.plot(data['times'],data['batt_heater']*5,label='Battery Heater',linestyle='-',color='c')
+    ax.plot([0,np.amax(times)],[heater_on,heater_on],'--g',label='Heater Set Point On')
+    ax.plot([0,np.amax(times)],[heater_off,heater_off],'--r',label='Heater Set Point Off')
     #ax1.plot(data['env_temp'],label="Environmental Temperature",marker='v')
     #ax1.plot(data['env_hum'],label="Environmental Humidity",marker='^')
-    ax1.set_xlim([np.amin(timelimits),np.amax(timelimits)])
-    ax1.legend()
-    
+    #ax.set_xlim([np.amin(timelimits),np.amax(timelimits)])
+    ax.set_xlabel("Time [s]")
+    ax.set_ylabel("Temperature [C]")
+    ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand", borderaxespad=0.)
+
   
     
     #dataplot = data.Series(data['spec_temp'])
@@ -152,19 +159,20 @@ def plotTemperatures(data):
     
 
     
-def plotADS(data):
-    fig,(ax1)=plt.subplots()    
+def plotADS(data,ax):
 
-    ax1.plot(data['ads1'],label='ADS 1')    
-    ax1.plot(data['ads2'],label='ADS 2')    
-    ax1.plot(data['ads3'],label='ADS 3')    
-    ax1.plot(data['ads4'],label='ADS 4')  
-    ax1.legend()
+    ax.plot(data['times'],data['ads1'],label='ADS 1')    
+    ax.plot(data['times'],data['ads2'],label='ADS 2')    
+    ax.plot(data['times'],data['ads3'],label='ADS 3')    
+    ax.plot(data['times'],data['ads4'],label='ADS 4')  
+    ax.set_xlabel("Time [s]")
+    ax.set_ylabel("ADS Value")
+    ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=4, mode="expand", borderaxespad=0.)
     
     
-def plotSpectrum(data,wavelength,spectrum):
+def plotSpectrum(data,wavelength,spectrum,timeToPlot,ax,axx):
     #fig,(ax1,ax2,ax3)=plt.subplots(3,1,sharex=True)    
-    fig,(ax1)=plt.subplots(1,1)    
+    #fig,(ax1)=plt.subplots(1,1)    
 
     #Measured Noise
     rmsNoise = np.std(spectrum[1:10,:],axis=0)
@@ -174,16 +182,26 @@ def plotSpectrum(data,wavelength,spectrum):
     poissonNoise = np.sqrt(aveSpectrum)
 
     #Measured SNR
-    measuredSNR = np.divide(aveSpectrum,rmsNoise)
+   # measuredSNR = np.divide(aveSpectrum,rmsNoise)
     
     #Expected SNR
-    poissonSNR = np.divide(aveSpectrum,poissonNoise)
+    #poissonSNR = np.divide(aveSpectrum,poissonNoise)
+    #times = data['times']
+    #times=np.array(times - times[0]) 
+    #nonzero=np.where(times>0)
+    #timelimits = times[nonzero]
     
-    whichspectrum = 1
-    
-    ax1.plot(wavelength[whichspectrum,:],spectrum[whichspectrum,:],'b')
-    ax1.set_xlim([350,1000])
-    plt.grid()
+    #whichspectrum = 70
+    #whichspectrum = np.argmax(data['times']>timeToPlot)
+    whichspectrum = np.where(data['times']>timeToPlot)[0][0]
+    plt.subplots_adjust(hspace=0.5)
+    axx.grid(True)    
+    axx.plot(wavelength[whichspectrum,:],spectrum[whichspectrum,:],'b')
+    axx.set_xlabel("Wavelength [nm]")
+    axx.set_ylabel("Counts")
+    axx.set_xlim([200,1000])
+    #axx.set_ylim([0,np.amax(spectrum[whichspectrum,:])*1.05])
+    axx.set_ylim([0,65536])
     #ax2.plot(wavelength[whichspectrum,:],rmsNoise,'b',label="Measured")
     #ax2.plot(wavelength[whichspectrum,:],poissonNoise,'r',label="Expected")
     #ax2.legend()
@@ -191,15 +209,19 @@ def plotSpectrum(data,wavelength,spectrum):
     #ax3.plot(wavelength[whichspectrum,:],poissonSNR,'r',label="Expected")
     #ax3.legend()
 
-    fig,(ax1)=plt.subplots(1,1)
-    times = data['times']
-    times=np.array(times - times[0]) 
-    nonzero=np.where(times>0)
-    timelimits = times[nonzero]
-    ax1.plot(times,np.amax(spectrum,axis=1))    
-    ax1.set_xlim([np.amin(timelimits),np.amax(timelimits)])
+    #fig,(ax1)=plt.subplots(1,1)
 
-def calibrateIrradiance(wavelength, spectrum):
+    ax.plot(data['times'],np.amax(spectrum,axis=1),'r')    
+
+    
+    ax.plot([data['times'][whichspectrum],data['times'][whichspectrum]],[0,65000],'b')
+    #ax3.set_xlim([np.amin(timelimits),np.amax(timelimits)])
+    ax.set_xlabel("Time [s]")
+    ax.set_ylabel("Peak Counts")
+    ax.set_ylim([0,np.amax(spectrum)*1.05])
+    ax.grid(True)
+   
+def calibrateIrradiance(wavelength, spectrum,ax1):
     
     calibration_file = "C:\\HAO-IG\\DIMS\\Software\\calibration_minimal.csv"
     calibration = np.genfromtxt(calibration_file,dtype='float',delimiter=',',skip_header=1)
@@ -213,7 +235,7 @@ def calibrateIrradiance(wavelength, spectrum):
     for spec in range(spectrum.shape[0]):
         irradiance[spec,:] = np.subtract(spectrum[spec,:],spectrum[10,:])
         irradiance[spec,:] = np.divide(irradiance[spec,:],cal2)  
-    fig,(ax1)=plt.subplots(1,1)    
+    #fig,(ax1)=plt.subplots(1,1)    
     print(wavelength[0,330])
     print(irradiance.shape)
     ax1.plot(wavelength[20,:],irradiance[20,:])
@@ -227,14 +249,24 @@ def saveData():
 
 
 def main():
+    timeToPlot = 10
     files = getFiles()
     data,wavelength,spectrum = getData(files)
     extractData()
-    plotTemperatures(data)
-    plotADS(data)
-    irradiance = calibrateIrradiance(wavelength,spectrum)
+
+    #fig,((ax1,ax3),(ax2,ax4))=plt.subplots(2,2)
+    fig = plt.subplots()
+    ax1=plt.subplot(321)
+    ax2=plt.subplot(323)
+    ax3=plt.subplot(325)
+    ax4=plt.subplot(122)
+    plotTemperatures(data,ax1)
+    plotADS(data,ax2)
+    #fig,(ax1,ax2,ax3)=plt.subplots(3,1)
+    #fig,(ax2,ax3)=plt.subplots(2,1)
+    #irradiance = calibrateIrradiance(wavelength,spectrum,ax1)
     #irradiance = spectrum    
-    plotSpectrum(data,wavelength,spectrum)
+    plotSpectrum(data,wavelength,spectrum,timeToPlot,ax3,ax4)
     saveData()
 
 
